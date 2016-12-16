04edd72cfae78aeccf7b9899a70c0ac5bd5364b9
Gordon Wiegand <Gordon.Wiegand@bluewin.ch>
2016-12-16 16:39:57 +0100

    fast fertig

--- makeParcels.r
+++ makeParcels.r
@@ -1,4 +1,4 @@
-getwd("/Volumes/hd2/mlakz")
+setwd("/Volumes/hd2/mlakz")
 rm(list=ls())
 
 refDate <- as.Date("2016-11-25")
@@ -7,6 +7,10 @@ noNeigh <- 5
 
 library(timeDate)
 d0 <- read.csv("./work/PADASA3.txt")
+dZub <- read.csv("./work/ZUBOFI3.txt")
+dZub$AKZ_VALID_TO <- as.POSIXlt(strptime(dZub$AKZ_VALID_TO , "%d/%m/%Y"))
+dZub <- dZub[is.na(dZub$AKZ_VALID_TO),]
+
 d0$dateTime <- as.POSIXlt(strptime(substr(d0$EVENTTIME,1,19), "%d/%m/%Y %H:%M:%S"))
 
 d0$mfm <- d0$dateTime$hour * 60 + d0$dateTime$min
@@ -19,7 +23,7 @@ noWeeks <- as.integer(ceiling(difftime(max(d0$dateTime), min(d0$dateTime),units=
 hkList <- unique(dRef$HOUSEKEY)
 out <- as.data.frame(matrix(nrow=length(hkList),ncol=14))
 for (i in length(hkList)){ 
-    dHouse <- d1[dRef$HOUSEKEY == hkList[i],]
+    dHouse <- d1[d1$HOUSEKEY == hkList[i],]
     outLoopInHouse <- numeric()
     for (id in 1:daysBack){
 
@@ -41,20 +45,76 @@ for (i in length(hkList)){
 
     }
     out[i,] <- outLoopInHouse
+    colnames(out) <- paste("in",namesWeekDays,sep="")
 
+    ###############################################3
+    posMax <- max(dZub$AKZ_POS[(dZub$AKZ_DISTR_ZIP == distZip) & (dZub$AKZ_TOUR_NR == tour)])
+    posAct <- dZub$AKZ_POS[dZub$AKZ_HAUSKEY == hkList[i]][1]
+
+
+
+    if ((posAct > noNeigh) & (posAct < posMax - noNeigh)) {
+        dTourZub <- dZub[((dZub$AKZ_DISTR_ZIP == distZip) & (dZub$AKZ_TOUR_NR == tour)),]
+        dTourZub <- dTourZub[((dTourZub$AKZ_POS >= (posAct - noNeigh)) & (dTourZub$AKZ_POS <= (posAct + noNeigh))),] 
+        dTourZub <- dTourZub[(dTourZub$AKZ_POS != posAct),]
+        lNeigh <- dTourZub$AKZ_HAUSKEY
+    }
+
+    if (posAct < noNeigh) {
+        dTourZub <- dZub[((dZub$AKZ_DISTR_ZIP == distZip) & (dZub$AKZ_TOUR_NR == tour)),] 
+        dTourZub <- dTourZub[(dTourZub$AKZ_POS %in% 1:10 ),] 
+        dTourZub <- dTourZub[(dTourZub$AKZ_POS != posAct),]
+        lNeigh <- dTourZub$AKZ_HAUSKEY
+    }
+
+
+    if (posAct > posMax - noNeigh) {
+        dTourZub <- dZub[((dZub$AKZ_DISTR_ZIP == distZip) & (dZub$AKZ_TOUR_NR == tour)),] 
+        dTourZub <- dTourZub[(dTourZub$AKZ_POS %in% ((posMax - 10):posMax)),] 
+        dTourZub <- dTourZub[(dTourZub$AKZ_POS != posAct),]
+        lNeigh <- dTourZub$AKZ_HAUSKEY
+    }
+    
+##########################3
+Hier überprüfen!!
+
+
+
+    dHouse <- d1[d1$HOUSEKEY %in% lNeigh,]
+    outLoopOutHouse <- numeric()
+    for (id in 1:daysBack){
+
+        if (!exists("namesWeekDays")){
+            week <- 0
+            namesWeekDays <- paste(dayOfWeek(timeDate(refDate - id)),week,sep="-")
+        } else {
+            if (isWeekday(timeDate(refDate - id))){
+                namesWeekDays <- c(namesWeekDays ,paste(dayOfWeek(timeDate(refDate - id)),week,sep="-"))
+            }
+            if (dayOfWeek(timeDate(refDate - id)) == "Fri"){
+                week <- week + 1
+            } 
+        }
+        index <- id - week * 2
+        if (isWeekday(timeDate(refDate - id))){
+            outLoopInHouse[index] <- median(dHouse$mfm[as.Date(dHouse$dateTime)==(refDate-id)])
+        } 
+
+    }
+    out[i,] <- outLoopInHouse
+    ###############################################3
 }
-colnames(out) <- paste("in",namesWeekDays,sep="")
-    
-    tour <- dRef$TOUR[dRef$HOUSEKEY == hkList[i]][1]    
-    distzip <- dRef$DISTRIBUTIONZIP[dRef$HOUSEKEY== hkList[i]][1]    
-	inRoute <- (d1$TOUR==tour & d1$DISTRIBUTIONZIP==distzip & d1$HOUSEKEY !=hkList[i])
-    
-    dLoopNei <- d1[inRoute,]
 
-    
-    
-    
-    out
+tour <- dRef$TOUR[dRef$HOUSEKEY == hkList[i]][1]    
+distzip <- dRef$DISTRIBUTIONZIP[dRef$HOUSEKEY== hkList[i]][1]    
+inRoute <- (d1$TOUR==tour & d1$DISTRIBUTIONZIP==distzip & d1$HOUSEKEY !=hkList[i])
+
+dLoopNei <- d1[inRoute,]
+
+
+
+
+out
 
 
 
--- spielplatz.r
+++ spielplatz.r
@@ -1,32 +1,91 @@
+	
+	
+d0 <- d0[(d0$DISTRIBUTIONZIP == unique(d0$DISTRIBUTIONZIP[1])),]  
+dim(d0)	
 
-i <- 1
-id <- 2
+# _____________________________________________
+dZub[ dZub$AKZ_HAUSKEY == max(table(dZub$AKZ_HAUSKEY)),]
+dim(dZub)
+colnames(dZub)
+table(dZub$AKZ_POS,exclude=NULL)
+dZub[dZub$AKZ_HAUSKEY %in% lNeigh,]
+table(dZub$AKZ_HAUSKEY==lNeigh)
+typeof(dZub$AKZ_HAUSKEY)
+table(is.element(dZub$AKZ_HAUSKEY,lNeigh))
+# _____________________________________________
+# _____________________________________________
+tour <- d1$TOUR[dRef$HOUSEKEY == hkList[i]][1]
+distZip <- d1$DISTRIBUTIONZIP[dRef$HOUSEKEY == hkList[i]][1]
 
-for (i in length(hkList)){ 
-	dHouse <- d1[dRef$HOUSEKEY == hkList[i],]
-	outLoopInHouse <- numeric()
-	for (id in 1:daysBack){
+dOutHouse <- d1[((d1$DISTRIBUTIONZIP == distZip) & 
+		(d1$HOUSEKEY != hkList[i]) &
+		(d1$TOUR == tour)),]
 
-		if (!exists("namesWeekDays")){
-			week <- 0
-			namesWeekDays <- paste(dayOfWeek(timeDate(refDate - id)),week,sep="-")
-		} else {
-				if (isWeekday(timeDate(refDate - id))){
-				namesWeekDays <- c(namesWeekDays ,paste(dayOfWeek(timeDate(refDate - id)),week,sep="-"))
-				}
-			if (dayOfWeek(timeDate(refDate - id)) == "Fri"){
-				week <- week + 1
-			} 
-		}
-		index <- id - week * 2
-		if (isWeekday(timeDate(refDate - id))){
-			outLoopInHouse[index] <- median(dHouse$mfm[as.Date(dHouse$dateTime)==(refDate-id)])
-		} 
 
-	}
-	out[i,] <- outLoopInHouse
+# _____________________________________________
+# _____________________________________________
 
-}
-colnames(out) <- paste("in",namesWeekDays,sep="")
-rm(week)
-rm(namesWeekDays)
+dTourZub$AKZ_VALID_TO <- as.POSIXlt(strptime(dTourZub$AKZ_VALID_TO , "%d/%m/%Y"))
+dTourZub <- dTourZub[is.na(dTourZub$AKZ_VALID_TO),]
+dTourZub <- dTourZub[(dTourZub$AKZ_POS != posAct),]
+
+
+outLoopInHouse <- numeric()
+
+    for (id in 1:daysBack){
+
+        if (!exists("namesWeekDays")){
+            week <- 0
+            namesWeekDays <- paste(dayOfWeek(timeDate(refDate - id)),week,sep="-")
+        } else {
+            if (isWeekday(timeDate(refDate - id))){
+                namesWeekDays <- c(namesWeekDays ,paste(dayOfWeek(timeDate(refDate - id)),week,sep="-"))
+            }
+            if (dayOfWeek(timeDate(refDate - id)) == "Fri"){
+                week <- week + 1
+            } 
+        }
+        index <- id - week * 2
+        if (isWeekday(timeDate(refDate - id))){
+            outLoopInHouse[index] <- median(dOutHouse$mfm[as.Date(dOutHouse$dateTime)==(refDate-id)])
+        } 
+
+    }
+    out[i,] <- outLoopInHouse
+    colnames(out) <- paste("in",namesWeekDays,sep="")
+
+# _____________________________________________
+    #  [1] "AKZ_DISTR_ZIP"    "AKZ_DISTR_OFFICE" "AKZ_DISTR_L"      "AKZ_DISTR_P"      "AKZ_TOUR_NR"      "AKZ_TOUR_LOG_KEY" "AKZ_ETP_NR"       "AKZ_ADRESS_ZIP"   "AKZ_HAUSKEY"      "AKZ_VALID_FROM"
+    # [11] "AKZ_VALID_TO"     "AKZ_POS"          "AKZ_DISTR_TYPE"
+    
+    
+    
+    dZub <- read.csv2('zubofi_oktober2.csv')
+
+            #Change NULL aof valid to to refDate
+    #             dZub$ZUB_AKZ_CHANGE_TO <- as.character(dZub$ZUB_AKZ_CHANGE_TO)
+    #             dZub$ZUB_AKZ_CHANGE_TO[dZub$ZUB_AKZ_CHANGE_TO=="NULL"] <- as.character(refDate)
+
+            dZub <- dZub[as.Date(dZub$ZUB_AKZ_CHANGE_TO) > refDate-21,]
+            selector <-  as.numeric(names(table(dZub$ZUB_AKZ_HAUSKEY)>1))
+            dZub$ZUB_AKZ_CHANGE_TO <- as.Date(dZub$ZUB_AKZ_CHANGE_TO)
+            dZub$ZUB_AKZ_DATE_CHANGE <- as.Date(dZub$ZUB_AKZ_DATE_CHANGE)
+
+            for (ii in 1:length(selector)){
+                dateMax <- max(dZub$ZUB_AKZ_CHANGE_TO[dZub$ZUB_AKZ_HAUSKEY==selector[ii]])
+                dZub <- dZub[!(dZub$ZUB_AKZ_HAUSKEY==selector[ii] & dZub$ZUB_AKZ_CHANGE_TO != dateMax),]
+            }
+            #only events after last reorganisation
+            d$PAD_EVENTDATE <- as.Date(d$PAD_EVENTDATE)
+            d$selector <- rep(F,nrow(d))
+            for (ii in 1:nrow(d)){
+                if (sum(dZub$ZUB_AKZ_HAUSKEY==d$PAD_HOUSEKEY[ii]) > 0) {
+                    d$selector[ii] <- d$PAD_EVENTDATE[ii]>dZub$ZUB_AKZ_DATE_CHANGE[dZub$ZUB_AKZ_HAUSKEY==d$PAD_HOUSEKEY[ii]]
+                }
+            }
+            d <- d[d$selector,]
+
+            outFrame <- as.data.frame(matrix(ncol=13,nrow=nrow(dZub)))#nrow(d_ref)))
+
+
+
